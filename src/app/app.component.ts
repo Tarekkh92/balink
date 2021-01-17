d5b323de590534682ba6be1e8ed840479a2e8478
Nitin <nitinvg97@gmail.com>
2021-01-17 16:35:15 +0530

    code update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomersService } from '../shared/services/customer.service';
-import{Customer} from '../shared/models/customer.model';
+import { Customer } from '../shared/models/customer.model';
 import { StepperSelectionEvent } from '@angular/cdk/stepper';
 
 
@@ -11,124 +11,145 @@ import { StepperSelectionEvent } from '@angular/cdk/stepper';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  buttonDisabled: Boolean=false;
- 
+  buttonDisabled: Boolean = false;
+  showNotification = false;
   name = 'balink';
-  validate:Boolean=false;
-  msg_firstName:string="";
-  msg_lastName:string="";
-  msg_title:string="";
-  msg_country:string="";
-  msg_city:string="";
-  msg_street:string="";
-  msg_email:string="";
-  msg_phone:string="";
+  error=true;
+  isSubmitted3: Boolean = false;
+  isSubmitted2: Boolean = false;
+  isSubmitted: Boolean = false;
+  isEditable: Boolean = false;
+  validate: Boolean = false;
+  msg_firstName: string = "";
+  msg_lastName: string = "";
+  msg_title: string = "";
+  msg_country: string = "";
+  msg_city: string = "";
+  msg_street: string = "";
+  msg_email: string = "";
+  msg_phone: string = "";
   msgSuccess: string = "";
   msgError: string = "";
- 
-  // firstFormGroup: FormGroup | undefined;
-  // secondFormGroup: FormGroup | undefined;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  isLinear = true;
   public customer: any = {};
 
-  
-   constructor(private _formBuilder: FormBuilder,private ps:CustomersService) {}
-  
+
+  constructor(private _formBuilder: FormBuilder, private ps: CustomersService) {
+    this.firstFormGroup = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      title: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      country: ['', Validators.required],
+      city: ['', Validators.required],
+      street: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      email: ['', [Validators.required,Validators.pattern('^[\w.]+@[\w.]+[\w.]{2,}$')]],
+      phone: ['', [Validators.required, Validators.pattern('^05[023458]-?[1-9]\d{6}$')]],
+      option: ['', Validators.required]
+    });
+  }
+
   ngOnInit() {
-   
-   
     this.ps.getCustomersAsync().subscribe(res => {
       console.log(res);
     });
-  //   this.firstFormGroup = this._formBuilder.group({
-  //     firstCtrl: ['', Validators.required]
-  //   });
-  //   this.secondFormGroup = this._formBuilder.group({
-  //     secondCtrl: ['', Validators.required]
-  //   });
-  // }
+  }
 
-}
-public firstForm(){
- 
-  // if(!this.customer.firstName){
-  //   this.msg_firstName = " First name is missing !";
-  // }
-  // if(!this.customer.lastName){
-  //   this.msg_lastName = " Last name is missing !  ";
-  // }
-  // if(!this.customer.title){
-  //   this.msg_title = " Title  is missing ! ";
-  // }
+  initialize(){
+    this.firstFormGroup = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      title: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      country: ['', Validators.required],
+      city: ['', Validators.required],
+      street: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+      option: ['', Validators.required]
+    });
+  }
 
-  if(this.customer.firstName!="" && this.customer.lastName !="" && this.customer != ""){
-   return true;
+  get formControls() {
+    return this.firstFormGroup.controls;
+  }
+  get formControls2() {
+    return this.secondFormGroup.controls;
+  }
+  get formControls3() {
+    return this.thirdFormGroup.controls;
+  }
 
+
+  public submitForm() {
+    console.log(this.thirdFormGroup)
+    this.error = false;
+    if (this.firstFormGroup.invalid || this.secondFormGroup.invalid || this.thirdFormGroup.invalid) {
+      this.error=true;
+      this.showNotification = true;
+        setTimeout(()=>{
+          this.showNotification = false;
+        },4000)
+      return;
+    }
+
+
+    // if(!this.customer.firstName ||  !this.customer.lastName  ||! this.customer.title  ||
+    //   !this.customer.country || !this.customer.city || !this.customer.street || !this.customer.email
+    //   || !this.customer.phone
+    //     ){
+    //   this.msgError += 'One of the Fields is missing ';
+    //   this.msg_firstName = " First name is missing !";
+    //   this.msg_lastName = " Last name is missing !  ";
+    //   this.msg_title = " Title  is missing ! ";
+    //   this.msg_country = " Country   is missing ! ";
+    //   this.msg_city = " City   is missing ! ";
+    //   this.msg_city = " City is missing ! ";
+    //   this.msg_street = " Street   is missing ! ";
+    //   this.msg_email = " Email   is missing ! ";
+    //   this.msg_phone = " Phone   is missing ! ";
+    // }
+    else {
+      let request = {
+        ...this.firstFormGroup.value,
+        ...this.secondFormGroup.value,
+        ...this.thirdFormGroup.value
+      }
+      this.ps.addCustomer(request).subscribe(res => {
+        this.initialize();
+        this.customer = {};
+        this.msgError = '';
+        this.showNotification = true;
+        setTimeout(()=>{
+          this.showNotification = false;
+        },4000)
+        // console.log('res from customer: ', res);
+        this.msgSuccess = 'Success Submission , Thank you For the Time. ';
+        setTimeout(() => {
+          window.location.href = '/';
+        }, 6000)
+
+      }, err => {
+        this.error=true;
+        this.showNotification = true;
+        setTimeout(()=>{
+          this.showNotification = false;
+        },4000)
+        console.log('error from customer: ', err)
+        this.msgError = 'Error';
+      })
+    }
 
   }
-  return false;
-}
-
-// public firstForm(){
-//   if(!this.customer.firstName){
-//     this.msg_firstName = " First name is missing !";
-//   }
-//   if(!this.customer.lastName){
-//     this.msg_lastName = " Last name is missing !  ";
-//   }
-//   if(!this.customer.title){
-//     this.msg_title = " Title  is missing ! ";
-//   }
-//   if(this.customer.firstName!="" && this.customer.lastName !="" && this.customer != ""){
-//    this.customer.firstName='';
-
-
-//   }
-  
-// }
-public submitForm() {
-   
-  // console.log('firstname: ', this.customer.firstName);
-  // console.log('lastname data: ', this.customer.lastName);
-  // console.log('title data: ', this.customer.title);
-  // console.log('title data: ', this.customer.country);
-  // console.log('title data: ', this.customer.city);
-  // console.log('title data: ', this.customer.street);
-  // console.log('title data: ', this.customer.email);
-  // console.log('title data: ', this.customer.phone);
-  // console.log('title data: ', this.customer.option);
-if(!this.customer.firstName ||  !this.customer.lastName  ||! this.customer.title  ||
-  !this.customer.country || !this.customer.city || !this.customer.street || !this.customer.email 
-  || !this.customer.phone
-    ){
-  this.msgError += 'One of the Fields is missing ';
-  this.msg_firstName = " First name is missing !";
-  this.msg_lastName = " Last name is missing !  ";
-  this.msg_title = " Title  is missing ! ";
-  this.msg_country = " Country   is missing ! ";
-  this.msg_city = " City   is missing ! ";
-  this.msg_city = " City is missing ! ";
-  this.msg_street = " Street   is missing ! ";
-  this.msg_email = " Email   is missing ! ";
-  this.msg_phone = " Phone   is missing ! ";
-}
-else{
-  this.ps.addCustomer(this.customer).subscribe(res => {
-  
-    this.customer = {};
-    this.msgError = '';
-    // console.log('res from customer: ', res);
-    this.msgSuccess = 'Success Submission , Thank you For the Time. ';
-    setTimeout(()=>{
-      window.location.href='/';
-    },2000)
-
-  }, err => {
-    console.log('error from customer: ', err)
-    this.msgError = 'Error';
-  })
-}
-  
-}
 
 
 }
